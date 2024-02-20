3ebc78f0e24e4a72dc0cc87401662a081b872658
Gwyn <rexdollente13@gmail.com>
2024-02-20 10:47:17 +0800

    Exercise 2.4 Course Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,86 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return <Course course={courses} />
 }
 
 const Course = ({course}) => {
-  const name = course.name
-  const parts = course.parts
 
   return (
     <>
-      <Header name={name} />
-      <Content parts={parts} />
-      <Total parts={parts}/>
+      {course.map(crs => 
+          <div key={crs.id}>
+            <Header name={crs.name} myKey={crs.id}/>
+            <Content parts={crs.parts} myKey={crs.id}/>
+            <Total parts={crs.parts} myKey={crs.id}/>
+          </div>
+        )}
     </> 
   );
 }
 
-const Header = ({name}) => <h1> {name} </h1>  
+const Header = ({name, myKey}) => <h2 key={myKey}> {name} </h2>  
 
-const Content = ({parts}) => {
+const Content = ({parts, myKey}) => {
   return(
-    <>
+    <div key={myKey}>
       {parts.map(part => <Part part={part} key={part.id}/>)}
-    </>
+    </div>
   )
 }
 
 const Part = ({part, myKey}) => <p key={myKey}>{part.name} {part.exercises}</p>
 
-const Total = ({parts}) => {
+const Total = ({parts, myKey}) => {
   // const exec = parts.map(a => a.exercises) // get exercises
   // const sum = exec.reduce((a,b) => a + b) // sum with reduce
   const sum2 = parts.reduce((a, b) => a + b.exercises,0) // initial val req
 
   return(
-    <b>total of {sum2} exercises</b>
+    <b key={myKey}>total of {sum2} exercises</b>
   )
 }
  
