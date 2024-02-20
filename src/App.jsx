e3d480fd525e609c19aaad43598be9b44962b2bf
Gwyn <rexdollente13@gmail.com>
2024-02-20 10:51:09 +0800

    Exercise 2.5 Course Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Course from './components/Course'
+
 const App = () => {
   const courses = [
     {
@@ -47,44 +49,4 @@ const App = () => {
   return <Course course={courses} />
 }
 
-const Course = ({course}) => {
-
-  return (
-    <>
-      {course.map(crs => 
-          <div key={crs.id}>
-            <Header name={crs.name} myKey={crs.id}/>
-            <Content parts={crs.parts} myKey={crs.id}/>
-            <Total parts={crs.parts} myKey={crs.id}/>
-          </div>
-        )}
-    </> 
-  );
-}
-
-const Header = ({name, myKey}) => <h2 key={myKey}> {name} </h2>  
-
-const Content = ({parts, myKey}) => {
-  return(
-    <div key={myKey}>
-      {parts.map(part => <Part part={part} key={part.id}/>)}
-    </div>
-  )
-}
-
-const Part = ({part, myKey}) => <p key={myKey}>{part.name} {part.exercises}</p>
-
-const Total = ({parts, myKey}) => {
-  // const exec = parts.map(a => a.exercises) // get exercises
-  // const sum = exec.reduce((a,b) => a + b) // sum with reduce
-  const sum2 = parts.reduce((a, b) => a + b.exercises,0) // initial val req
-
-  return(
-    <b key={myKey}>total of {sum2} exercises</b>
-  )
-}
- 
-
-
- 
 export default App
