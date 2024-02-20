4633e0aabe5ee6f8d8f6ac14d0cf54a6e4c10f03
Gwyn <rexdollente13@gmail.com>
2024-02-20 20:46:31 +0800

    Exercises 2.8 Phonebook Complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState(0)
 
   const addNewName = (event) => {
     event.preventDefault()
     let personExists = persons.find(p => p.name === newName) // returns object
-    console.log(personExists ? personExists : false)
+    // console.log(personExists ? personExists : false)
 
     if(personExists)
-      window.alert(`${newName} is already added to`)
+      window.alert(`${newName} is already added to phonebook`)
     else {
       const pTemp = [...persons]
-      setPersons(pTemp.concat({name:newName}))
+      setPersons(pTemp.concat({name:newName, number:newNum}))
     }
   }
 
@@ -23,7 +24,9 @@ const App = () => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
-   
+
+  const handleNewNum = (event) => setNewNum(event.target.value)
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -32,6 +35,11 @@ const App = () => {
           name: <input 
           value={newName} 
           onChange={handleNameChage}/>
+        </div>
+        <div>
+          number: <input 
+          value={newNum}
+          onChange={handleNewNum}/>
         </div>
         <div>
           <button 
@@ -45,7 +53,7 @@ const App = () => {
       <div>
         {persons.map(p => 
           <span key={p.name}>
-            {p.name}
+            {p.name} {p.number}
           <br/></span>
         )}
       </div>
